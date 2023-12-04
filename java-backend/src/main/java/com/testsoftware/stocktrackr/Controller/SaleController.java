68627c7e16221a3683f0f097ce525e3b122b0a1d
BrnCosta <brunooc.bc@gmail.com>
2023-12-04 09:42:03 -0300

    Adding java code coverage

--- java-backend/src/main/java/com/testsoftware/stocktrackr/Controller/SaleController.java
+++ java-backend/src/main/java/com/testsoftware/stocktrackr/Controller/SaleController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testsoftware.stocktrackr.Models.*;
 import com.testsoftware.stocktrackr.Service.StockService;
 
